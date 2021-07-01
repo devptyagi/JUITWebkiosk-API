12653024689a0e510cb72fac10cb18179c769f6c
devptyagi <56474716+devptyagi@users.noreply.github.com>
2021-07-01 14:30:29 +0530

    Changed Subject Faculty Error Response

--- src/main/java/com/devtyagi/juitwebkiosk/controller/SubjectFacultyController.java
+++ src/main/java/com/devtyagi/juitwebkiosk/controller/SubjectFacultyController.java
@@ -42,11 +42,13 @@ public class SubjectFacultyController {
             List<SubjectFaculty> res = SubjectFacultyProcessor.getSubjectFaculty(enrollmentNumber, password, subjectFacultyUrl);
             return new ResponseEntity<>(res, HttpStatus.OK);
         } catch (InvalidCredentialsException e) {
-            Map<String, String> res = new HashMap<>();
+            Map<String, Object> res = new HashMap<>();
+            res.put("success", false);
             res.put("error", "Invalid Credentials / Session Timed Out");
             return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
         } catch (IOException e) {
-            Map<String, String> res = new HashMap<>();
+            Map<String, Object> res = new HashMap<>();
+            res.put("success", false);
             res.put("error", "Unknown Error");
             return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
         }
