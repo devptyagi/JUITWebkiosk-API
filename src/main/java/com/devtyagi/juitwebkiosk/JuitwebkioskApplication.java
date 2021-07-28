a61a02b5f6bd9f44e720bc3e294026ee6dbb1063
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2021-07-28 11:01:48 +0530

    Added CORS Configuration

--- src/main/java/com/devtyagi/juitwebkiosk/JuitwebkioskApplication.java
+++ src/main/java/com/devtyagi/juitwebkiosk/JuitwebkioskApplication.java
@@ -3,6 +3,8 @@ package com.devtyagi.juitwebkiosk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -17,6 +19,16 @@ public class JuitwebkioskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JuitwebkioskApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 	@Bean
